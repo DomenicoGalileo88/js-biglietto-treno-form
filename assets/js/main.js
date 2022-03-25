c9e14d7ac1b004f7dcfd5b6160870b8842bc148b
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-25 16:38:23 +0100

    Add value

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,17 +9,17 @@ Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato
 let prezzo_finale, messaggio;
 const prezzo_al_km = 0.21;
 
+//chiedere all'utente di digitare il suo nome
+function fullName() {
+    const x = document.getElementById('full_name').value;
+    document.getElementById("nome_passeggero").innerHTML = x;
+}
 //chiedere all'utente il numero di chilometri 
 const km_da_percorrere = document.getElementById("km_da_percorrere").value; 
 
-if (isNaN(km_da_percorrere)) {
-    alert('😱 Devi inserire un numero per i km da percorrere')
-}
 //chiedere all'utente età del passeggero.
 const eta_passeggero = document.getElementById('sconti').value;
-if (isNaN(eta_passeggero)) {
-    alert('😱 Devi inserire un numero per l\'etá passeggero')
-}
+
 //console.log(km_da_percorrere, eta_passeggero);
 
 /*
@@ -40,26 +40,26 @@ const prezzo_over_65 = prezzo_standard - (prezzo_standard * 0.4)
 //console.log(prezzo_minorenni, prezzo_over_65);
 
 
-if (eta_passeggero < 18) {
-    messaggio = 'Sei minorenne hai diritto al prezzo scontato del 20%';
+if (eta_passeggero === 'Minorenne') {
+ //   messaggio = 'Sei minorenne hai diritto al prezzo scontato del 20%';
     prezzo_finale = prezzo_minorenni;
 
-} else if (eta_passeggero > 65) {
-    messaggio = 'Sei un over 65 hai diritto al prezzo scontato del 40%';
+} else if (eta_passeggero === 'Over 65') {
+   // messaggio = 'Sei un over 65 hai diritto al prezzo scontato del 40%';
     prezzo_finale = prezzo_over_65
 } else {
-    messaggio = '😢 Sorry! prezzo pieno';
+   // messaggio = '😢 Sorry! prezzo pieno';
     prezzo_finale = prezzo_standard
 }
 
 // L'output del prezzo finale va scritto in forma umana
 //console.log(`${messaggio}, prezzo biglietto: Euro ${prezzo_finale.toFixed(2)} `);
 console.log(prezzo_finale);
-if (isNaN(prezzo_finale)) {
+/* if (isNaN(prezzo_finale)) {
     document.getElementById('prezzo_biglietto').innerHTML = `😱 Riprova, hai inserito dei dati sbagliati`
 
 } else {
 
-    document.getElementById('prezzo_biglietto').innerHTML = `${messaggio}, prezzo biglietto: Euro ${prezzo_finale.toFixed(2)} `
+    document.getElementById('prezzo_biglietto').innerHTML = `prezzo biglietto: Euro ${prezzo_finale.toFixed(2)} `
 }
- 
+  */

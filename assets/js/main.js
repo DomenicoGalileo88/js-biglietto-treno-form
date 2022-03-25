0ef1028387106b7a1c5864d1e144159dd8da46e7
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-25 17:25:13 +0100

    add click

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,55 +6,60 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
  */
+
+
 let prezzo_finale, messaggio;
 const prezzo_al_km = 0.21;
+//immagazzinare dati utente
+const full_name = document.getElementById('full_name').value;
 
-//chiedere all'utente di digitare il suo nome
-function fullName() {
-    const x = document.getElementById('full_name').value;
-    document.getElementById("nome_passeggero").innerHTML = x;
-}
-//chiedere all'utente il numero di chilometri 
-const km_da_percorrere = document.getElementById("km_da_percorrere").value; 
 
-//chiedere all'utente età del passeggero.
+// km da percorrere
+const km_da_percorrere = document.getElementById("km_da_percorrere").value;
+
+// età passeggero
 const eta_passeggero = document.getElementById('sconti').value;
 
-//console.log(km_da_percorrere, eta_passeggero);
+    //console.log(km_da_percorrere, eta_passeggero);
 
-/*
-calcolare il prezzo totale del viaggio:
-- prezzo per km 0.21
-- 20% di sconto minorenni
-- 40% per gli over 65.
-*/
+//utilità button genera
+const genera = document.getElementById('genera');
+genera.addEventListener('click', function () {
+
+   full_name.getElementById("nome_passeggero").innerHTML = 'full_name';
+
+/*     // calcolo prezzo standard
+    const prezzo_standard = km_da_percorrere * prezzo_al_km;
+    //console.log(prezzo_standard);
+
+    //calcolo sconti
+    const prezzo_minorenni = prezzo_standard - (prezzo_standard * 0.2)
+    const prezzo_over_65 = prezzo_standard - (prezzo_standard * 0.4)
+    //console.log(prezzo_minorenni, prezzo_over_65);
+
+    if (eta_passeggero == 'Minorenne') {
+        //   messaggio = 'Sei minorenne hai diritto al prezzo scontato del 20%';
+        prezzo_finale = prezzo_minorenni;
+
+    } else if (eta_passeggero == 'Over 65') {
+        // messaggio = 'Sei un over 65 hai diritto al prezzo scontato del 40%';
+        prezzo_finale = prezzo_over_65
+    } else {
+        // messaggio = '😢 Sorry! prezzo pieno';
+        prezzo_finale = prezzo_standard
+    }
+    console.log(prezzo_finale); */
+});
 
 
 
 
-const prezzo_standard = km_da_percorrere * prezzo_al_km;
-//console.log(prezzo_standard);
-
-const prezzo_minorenni = prezzo_standard - (prezzo_standard * 0.2)
-const prezzo_over_65 = prezzo_standard - (prezzo_standard * 0.4)
-//console.log(prezzo_minorenni, prezzo_over_65);
 
 
-if (eta_passeggero === 'Minorenne') {
- //   messaggio = 'Sei minorenne hai diritto al prezzo scontato del 20%';
-    prezzo_finale = prezzo_minorenni;
-
-} else if (eta_passeggero === 'Over 65') {
-   // messaggio = 'Sei un over 65 hai diritto al prezzo scontato del 40%';
-    prezzo_finale = prezzo_over_65
-} else {
-   // messaggio = '😢 Sorry! prezzo pieno';
-    prezzo_finale = prezzo_standard
-}
 
 // L'output del prezzo finale va scritto in forma umana
 //console.log(`${messaggio}, prezzo biglietto: Euro ${prezzo_finale.toFixed(2)} `);
-console.log(prezzo_finale);
+/* console.log(prezzo_finale); */
 /* if (isNaN(prezzo_finale)) {
     document.getElementById('prezzo_biglietto').innerHTML = `😱 Riprova, hai inserito dei dati sbagliati`
 

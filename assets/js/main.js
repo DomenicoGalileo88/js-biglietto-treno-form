724de12f57d8665b01c124bc127af1529907a7e4
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-26 11:44:07 +0100

    Add css

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,9 @@ const prezzo_al_km = 0.21;
 const genera = document.getElementById('genera');
 genera.addEventListener('click', function () {
 
+    //Appare la sezione dedicata al biglietto
+    document.getElementById('dati_biglietto').style.display = "block";
+
     // nome del passeggero (quello che inserisce l'utente )
     const fullName = document.getElementById("full_name").value;
     document.getElementById("nome_passeggero").innerHTML = fullName;
@@ -49,22 +52,19 @@ genera.addEventListener('click', function () {
 
     if (scontoApplicato.value == 'Minorenne' ) {
         //console.log(prezzo_minorenni);
-
         document.getElementById('costo_biglietto').innerHTML = `${prezzo_minorenni.toFixed(2)}€`;
     } else if (scontoApplicato.value == 'Over 65'){
         //console.log(prezzo_over_65);
-
         document.getElementById('costo_biglietto').innerHTML = `${prezzo_over_65.toFixed(2)}€`;
     } else{
         //console.log(prezzo_standard);
-
         document.getElementById('costo_biglietto').innerHTML = `${prezzo_standard.toFixed(2)}€`;
     }
 
 });
 /* #endregion click genera */
 
-
+// funzione bbutton Annulla
 const annulla = document.getElementById('annulla');
 
 annulla.addEventListener('click', function RefreshWindow() {
